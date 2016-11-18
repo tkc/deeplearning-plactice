d72826cacf224de3c40497d86e71d2fb7972bbb0
tkc <loosefingers@gmail.com>
2016-11-18 17:42:02 +0900

    Update

--- perceptron.py
+++ perceptron.py
@@ -10,18 +10,6 @@ def AND(x1, x2):
         return 1
 
 
-def ANDBIAS(x1, x2):
-    x = np.array([x1, x2])
-    w = np.array([0.5, 0.5])
-    b = -   0.7
-    tmp = np.sum(w * x) + b
-
-    if tmp <= 0:
-        return 0
-    else:
-        return 1
-
-
 def NAND(x1, x2):
     x = np.array([x1, x2])
     w = np.array([-0.5, -0.5])
@@ -36,7 +24,7 @@ def NAND(x1, x2):
 
 def OR(x1, x2):
     x = np.array([x1, x2])
-    w = np.array([-0.5, -0.5])
+    w = np.array([0.5, 0.5])
     b = - 0.2
     tmp = np.sum(w * x) + b
 
@@ -54,26 +42,26 @@ def XOR(x1, x2):
 
 
 if __name__ == '__main__':
+
+    #AND
     print  AND(0, 0)  # 0
     print  AND(1, 0)  # 0
     print  AND(0, 1)  # 0
     print  AND(1, 1)  # 1
 
-    print  ANDBIAS(0, 0)  #
-    print  ANDBIAS(1, 0)  #
-    print  ANDBIAS(0, 1)  #
-    print  ANDBIAS(1, 1)  #
-
+    #NAND
     print  NAND(0, 0)  # 1
     print  NAND(1, 0)  # 1
     print  NAND(0, 1)  # 1
     print  NAND(1, 1)  # 0
 
-    print  OR(0, 0)  # o
+    #OR
+    print  OR(0, 0)  # 0
     print  OR(1, 0)  # 1
     print  OR(0, 1)  # 1
     print  OR(1, 1)  # 1
 
+    #XOR
     print  XOR(0, 0)  # 0
     print  XOR(1, 0)  # 1
     print  XOR(0, 1)  # 1
