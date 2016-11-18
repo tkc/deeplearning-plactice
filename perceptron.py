936e17fdbe6858eae1a10840257ef1f3bfc8c35b
tkc <loosefingers@gmail.com>
2016-11-18 17:09:01 +0900

    Add NAND OR

--- perceptron.py
+++ perceptron.py
@@ -1,15 +1,70 @@
+import numpy as np
+
+
 def AND(x1, x2):
     w1, w2, theta = 0.5, 0.5, 0.7
     tmp = x1 * w1 + x2 * w2
+
     if tmp <= theta:
-        return 0;
+        return 0
     elif tmp > theta:
-        return 1;
+        return 1
+
+
+def ANDBIAS(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -   0.7
+    tmp = np.sum(w * x) + b
+
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+
+def NAND(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([-0.5, -0.5])
+    b = 0.7
+    tmp = np.sum(w * x) + b
+
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+def OR(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([-0.5, -0.5])
+    b = - 0.2
+    tmp = np.sum(w * x) + b
+
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
 
 if __name__ == '__main__':
-    print 'perceptron sample';
+    print 'perceptron samples';
 
-    print  AND(0, 0);
-    print  AND(1, 0);
-    print  AND(0, 1);
-    print  AND(1, 1);
+    print  AND(0, 0)
+    print  AND(1, 0)
+    print  AND(0, 1)
+    print  AND(1, 1)
+
+    print  ANDBIAS(0, 0)
+    print  ANDBIAS(1, 0)
+    print  ANDBIAS(0, 1)
+    print  ANDBIAS(1, 1)
+
+    print  NAND(0, 0)
+    print  NAND(1, 0)
+    print  NAND(0, 1)
+    print  NAND(1, 1)
+
+    print  OR(0, 0)
+    print  OR(1, 0)
+    print  OR(0, 1)
+    print  OR(1, 1)
