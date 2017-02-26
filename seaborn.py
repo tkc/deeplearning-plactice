ca70a650b08e399e16e540f6ceaf0c3247641ca5
Tkc <loosefingers@gmail.com>
2017-02-26 23:43:19 +0900

    Update sample

--- seaborn.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import pandas as pd
-import seaborn as sns
-
-x = np.random.normal(size=100)
-
-# titanic = sns.load_dataset("titanic")
-# tips = sns.load_dataset("tips")
-# iris = sns.load_dataset("iris")
